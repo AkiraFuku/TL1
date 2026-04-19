ecc7c5412412f78660ca8d7999b71daec0192a04
AkiraFuku <k024g1002@g.neec.ac.jp>
2026-04-19 19:51:23 +0900

    TL1_01_04

--- TL1_Addon/level_editor.py
+++ TL1_Addon/level_editor.py
@@ -11,12 +11,42 @@ bl_info={
     "tracker_url": "",
     "category": "Object"
 }
-
+#トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types. Menu):
+    #Blenderがクラスを識別する為の固有の文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+    #メニューのラベルとして表示される文字列
+    bl_label = "MyMenu"
+    #著者表示用の文字列
+    bl_description = "拡張メニュー by " + bl_info["author"]
+    # サブメニューの描画
+    def draw(self, context):
+    #トップバーの「エディターメニュー」に項目(オペレータ)を追加 
+         self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        #既存のメニューにサブメニューを追加
+    def submenu(self, context):
+    # ID指定でサブメニューを追加
+            self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+classes=(TOPBAR_MT_my_menu,)
+#メニュー項目
+def draw_menu_manual(self, context):
+    self.layout.operator("wm.url_open_preset",text="manual",icon='HELP')
 def register():
+    for cls in classes:
+         bpy.utils.register_class(cls)
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました。")
 
 def unregister():
+    for cls in classes:
+         bpy.utils.unregister_class(cls)
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが無効化されました。")
+
+
+
+
 
 if __name__ == "__main__":
     register()
+
