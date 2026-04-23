32d1f7b44dc33839d9b3c96b5e24c8af1f9256d8
AkiraFuku <k024g1002@g.neec.ac.jp>
2026-04-23 11:58:00 +0900

    TL1_01_05

--- TL1_Addon/level_editor.py
+++ TL1_Addon/level_editor.py
@@ -11,6 +11,34 @@ bl_info={
     "tracker_url": "",
     "category": "Object"
 }
+
+# オペレータ 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types. Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0 
+        print("頂点を伸ばしました。")
+        #オペレータの命令終了を通知
+        return {'FINISHED'}   
+# オペレータ ICO球生成
+class MYADDON_OT_ICO_sphere(bpy.types. Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました")
+        #オペレータの命令終了を通知
+        return {'FINISHED'}   
+    
 #トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types. Menu):
     #Blenderがクラスを識別する為の固有の文字列
@@ -22,15 +50,24 @@ class TOPBAR_MT_my_menu(bpy.types. Menu):
     # サブメニューの描画
     def draw(self, context):
     #トップバーの「エディターメニュー」に項目(オペレータ)を追加 
-         self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
+         self.layout.operator(MYADDON_OT_ICO_sphere.bl_idname, text=MYADDON_OT_ICO_sphere.bl_label)
         #既存のメニューにサブメニューを追加
     def submenu(self, context):
     # ID指定でサブメニューを追加
             self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
-classes=(TOPBAR_MT_my_menu,)
+classes=(
+     MYADDON_OT_ICO_sphere,
+     MYADDON_OT_stretch_vertex,
+     TOPBAR_MT_my_menu,
+     )
+#class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+
+     
 #メニュー項目
 def draw_menu_manual(self, context):
     self.layout.operator("wm.url_open_preset",text="manual",icon='HELP')
+
 def register():
     for cls in classes:
          bpy.utils.register_class(cls)
