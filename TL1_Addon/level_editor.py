b2948ab2829436e95ac75c1e9c62f89a9804ccc2
AkiraFuku <k024g1002@g.neec.ac.jp>
2026-06-08 23:10:56 +0900

    TL1_01_10

--- TL1_Addon/level_editor.py
+++ TL1_Addon/level_editor.py
@@ -4,6 +4,7 @@ import bpy_extras
 import gpu
 import gpu_extras.batch
 import copy
+import mathutils
 bl_info={
     "name": "レベルエディタ",
     "author": "Fuku Akira",
@@ -16,6 +17,37 @@ bl_info={
     "tracker_url": "",
     "category": "Object"
 }
+
+#オペレーター　カスタムプロパティ['collider7]追加
+class MYADDON_OT_add_collider(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_collider"
+    bl_label = "Collider 追加"
+    bl_description = "['collider'] カスタムプロパティを追加します"
+    bl_options = {"REGISTER", "UNDO"}
+    def execute(self, context):
+        #['collider'] カスタムプロパティを追加
+        context.object["collider"] = "BOX"
+        context.object["collider_center"]= mathutils.Vector((0,0,0))
+        context.object["collider_size"]= mathutils.Vector((2,2,2))
+        return {"FINISHED"}
+    
+class OBJECT_PT_collider(bpy.types.Panel):
+    """オブジェクトのコライダーパネル"""
+    bl_idname = "OBJECT_PT_collider"
+    bl_label = "Collider"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    def draw(self, context):
+        if  "collider" in context.object:
+            layout = self.layout
+            layout.prop(context.object, '["collider"]', text="Type")
+            layout.prop(context.object, '["collider_center"]', text="Collider Center")
+            layout.prop(context.object, '["collider_size"]', text="Collider Size")
+        else:
+            #プロパティがなければ、プロパティ追加ボタンを表示
+            self.layout.operator(MYADDON_OT_add_collider.bl_idname,text="Collider 追加")
 
 #コライダー描画
 class DrawCollider:
@@ -39,27 +71,42 @@ class DrawCollider:
         size =[2,2,2]
 
         for object in bpy.context.scene.objects:
+            if not "collider"  in object:
+                continue
+
+            center =mathutils.Vector((0,0,0))
+            size =mathutils.Vector((2,2,2))
+
+            center[0] =object["collider_center"][0]
+            center[1] =object["collider_center"][1]
+            center[2] =object["collider_center"][2]
+            size[0] =object["collider_size"][0]
+            size[1] =object["collider_size"][1]         
+            size[2] =object["collider_size"][2]
+
+
             start=len(vertices["pos"])
             for ofset in ofsets:
-                pos =copy.copy(object.location)
+                pos =copy.copy(center)
                 pos.x += ofset[0] * size[0]
                 pos.y += ofset[1] * size[1]
                 pos.z += ofset[2] * size[2]
+
+                pos =object.matrix_world @ pos
                 vertices["pos"].append(pos)
-                indices.append([start + 0, start + 1])
-                indices.append([start + 2, start + 3])
-                indices.append([start + 0, start + 2])
-                indices.append([start + 1, start + 3])
+            indices.append([start + 0, start + 1])
+            indices.append([start + 2, start + 3])
+            indices.append([start + 0, start + 2])
+            indices.append([start + 1, start + 3])
 
-                indices.append([start + 4, start + 5])
-                indices.append([start + 6, start + 7])
-                indices.append([start + 4, start + 6])
-                indices.append([start + 5, start + 7])
-
-                indices.append([start + 0, start + 4])
-                indices.append([start + 1, start + 5])
-                indices.append([start + 2, start + 6])
-                indices.append([start + 3, start + 7])
+            indices.append([start + 4, start + 5])
+            indices.append([start + 6, start + 7])
+            indices.append([start + 4, start + 6])
+            indices.append([start + 5, start + 7])
+            indices.append([start + 0, start + 4])
+            indices.append([start + 1, start + 5])
+            indices.append([start + 2, start + 6])
+            indices.append([start + 3, start + 7])
 
         shader =gpu.shader.from_builtin("UNIFORM_COLOR")
 
@@ -85,6 +132,7 @@ class OBJECT_PT_file_name(bpy.types.Panel):
      self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname)
      self.layout.operator(MYADDON_OT_ICO_sphere.bl_idname,text=MYADDON_OT_ICO_sphere.bl_label)
      self.layout.operator(MYADDON_OT_export_scene.bl_idname,text=MYADDON_OT_export_scene.bl_label)
+     
 
      if "file_name" in context.object:
          # 既にプロパティがあれば、プロパティを表示
@@ -184,6 +232,14 @@ class MYADDON_OT_export_scene(bpy.types. Operator, bpy_extras.io_utils.ExportHel
         #カスタムプロパティ
         if "file_name" in object:
             self.Write_and_print(file, indent + "Name %s" % object["file_name"])
+        if "collider" in object:
+             self.Write_and_print(file, indent + "Collider %s" % object["collider"])
+             temp_str= indent +"CC(%f,%f,%f)"
+             temp_str =temp_str % (object["collider_center"][0], object["collider_center"][1], object["collider_center"][2])
+             self.Write_and_print(file, temp_str)
+             temp_str= indent +"CS(%f,%f,%f)"
+             temp_str =temp_str % (object["collider_size"][0], object["collider_size"][1], object["collider_size"][2])
+             self.Write_and_print(file, temp_str)
         self.Write_and_print(file, indent + 'End')
         self.Write_and_print(file,'')
         #子オブジェクトがある場合は再帰的に解析
@@ -205,6 +261,7 @@ class TOPBAR_MT_my_menu(bpy.types. Menu):
          self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
          self.layout.operator(MYADDON_OT_ICO_sphere.bl_idname, text=MYADDON_OT_ICO_sphere.bl_label)
          self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
+         
         #既存のメニューにサブメニューを追加
     def submenu(self, context):
     # ID指定でサブメニューを追加
@@ -218,7 +275,9 @@ classes=(
      MYADDON_OT_stretch_vertex,
     
      MYADDON_OT_add_filwname,
-     OBJECT_PT_file_name
+     OBJECT_PT_file_name,
+     MYADDON_OT_add_collider,
+     OBJECT_PT_collider,
      )
 #class MYADDON_OT_stretch_vertex(bpy.types.Operator):
 
@@ -235,9 +294,9 @@ def register():
     print("レベルエディタが有効化されました。")
 
 def unregister():
-    
-    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
     bpy.types.SpaceView3D.draw_handler_remove(DrawCollider.handle,"WINDOW")
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+   
     for cls in classes:
          bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
